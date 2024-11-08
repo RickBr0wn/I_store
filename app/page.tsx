9138f766663768bcbcf8b12b1a9a7e98295fd4f2
Rick Brown <ricky.brown.00@gmail.com>
2024-11-08 22:47:31 +0000

    adds global css and global colours

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
 export default function Home() {
   return (
     <div>
-      <h1>CloudBox - The only storage solution you will need.</h1>
+      <h1 className="text-3xl text-brand font-bold">
+        CloudBox - The only storage solution you will need.
+      </h1>
     </div>
   )
 }
